52174c1a86f06f4b2b6e46f291b983ddbce3a4ae
Vinicius Baggio <vbaggio08@gmail.com>
2026-03-04 14:12:50 -0300

    enviado para a host

--- src/config/database.php
+++ src/config/database.php
@@ -5,9 +5,17 @@ class Database {
     public static function getConnection() {
         if (!self::$pdo) {
             try {
-                // Ajuste aqui se seu banco tiver senha
-                self::$pdo = new PDO('mysql:host=localhost;dbname=ripfire_db;charset=utf8', 'root', '');
+                // --- COLOQUE AQUI AS CREDENCIAIS DO BANCO DE DADOS DA HOSTGATOR ---
+                $host = 'localhost'; // ou o host que a HostGator fornecer
+                $dbname = 'vin31871_ripfire';
+                $user = 'vin31871_ripfire';
+                $password = 'Vb357753@';
+                // --------------------------------------------------------------------
+
+                self::$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
                 self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                // Desativa o modo ONLY_FULL_GROUP_BY para compatibilidade com o servidor da HostGator
+                self::$pdo->exec("SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''))");
             } catch (PDOException $e) {
                 die("Erro de conexão: " . $e->getMessage());
             }
