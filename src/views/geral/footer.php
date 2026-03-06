09b832bc0406000941075ea6b4beb0dc634b67bd
Vinicius Baggio <vbaggio08@gmail.com>
2026-03-06 11:15:25 -0300

    Corrigir dropdown Bootstrap - remover conflitos de CSS e adicionar inicialização de dropdowns no JavaScript

--- src/views/geral/footer.php
+++ src/views/geral/footer.php
@@ -9,5 +9,16 @@
     <!-- JavaScript do Bootstrap (Essencial para componentes como menu dropdown, modais, etc) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
+    <script>
+        // Garantir que os dropdowns do Bootstrap funcionem
+        document.addEventListener('DOMContentLoaded', function() {
+            // Inicializar todos os dropdowns
+            const dropdownElements = document.querySelectorAll('[data-bs-toggle="dropdown"]');
+            dropdownElements.forEach(element => {
+                new bootstrap.Dropdown(element);
+            });
+        });
+    </script>
+
 </body>
 </html>
