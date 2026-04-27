ff60a6cdb89b301f9a0bf06f35676849d6358261
Vinicius Baggio <vbaggio08@gmail.com>
2026-04-27 17:49:13 -0300

    feat: adiciona favicon com logo Ripfire na aba do navegador

--- src/views/geral/header.php
+++ src/views/geral/header.php
@@ -10,6 +10,7 @@ $isAdmin = (isset($_SESSION['user_nivel']) && $_SESSION['user_nivel'] == 'admin'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ripfire System</title>
+    <link rel="icon" type="image/png" href="<?= BASE_URL ?>assets/img/logo_rip.png">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
